4c818036948633ce373eacf0f240f88141f27483
Jessica <2dam@HZ375754.TartangaLH.eus>
2022-10-27 12:02:28 +0200

    SignUp controller en ejecucion

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -8,6 +8,8 @@ package controller;
 import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -41,27 +43,49 @@ public class SignUpController implements Initializable {
     @FXML
     private Label lblError2;
 
-    
-    
     @FXML
+    private static final String email=  "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(email);
+    @FXML
+    
+    
     private void handleButtonAction(ActionEvent event) {
-       
+
         //Validar que los campos nombre de usuario, fullname, 
         //email, password y confirmPasswd estén informados.
         try {
             if (this.txtNombre2.getText().isEmpty() || this.txtNombreComp.getText().isEmpty()
                     || this.txtEmail.getText().isEmpty() || this.txtPasswd2.getText().isEmpty()
                     || this.txtConfirmPasswd.getText().isEmpty()) {
-
-            } else {
                 //Si no están informados alguno de los campos saldrá un label de error (lblError2).
                 lblError2.setVisible(true);
                 lblError2.setText("");
+            } else {
+
+                //Validar que el máximo número de caracteres en el campo de nombre de usuario, fullname, password y confirmPassword sea de 15.     
+                if (this.txtNombre2.getText().length() > 15 || this.txtPasswd2.getText().length() > 15) {
+                    lblError2.setVisible(true);
+                    lblError2.setText("NUMERO CARACTERES \n INCORRECTOS");
+
+                } else {
+                    String recoger = this.txtEmail.toString();
+                    if (EMAIL_PATTERN.matcher(email).matches()){
+                        lblError2.setVisible(true);
+                        lblError2.setText("NUMERO CARACTERES \n INCORRECTOS");
+                        
+                    }else {
+                        if (txtPasswd2==txtConfirmPasswd) {
+                            lblError2.setVisible(true);
+                            lblError2.setText("LAS PASSWORD NO COINCIDEN");
+                        }
+                    }
+                    
+
+                    
+                }
 
                 //Seguido, saldrá del método del botón.
-                
-                
-                
             }
         } catch (Exception e) {
 
