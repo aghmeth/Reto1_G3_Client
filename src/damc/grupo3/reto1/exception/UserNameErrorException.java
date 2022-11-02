d2f1ff2c94337cd89bc15312c40a733667787642
2dam <2dam@HZ375744.TartangaLH.eus>
2022-11-02 13:49:45 +0100

    todo ok

---
+++ src/damc/grupo3/reto1/exception/UserNameErrorException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package damc.grupo3.reto1.exception;
+
+/**
+ *
+ * @author 2dam
+ */
+public class UserNameErrorException extends Exception{
+    
+     private static final long serialVersionUID = 1L;
+     
+      public UserNameErrorException (String message){
+        super(message);
+    }
+    
+}
